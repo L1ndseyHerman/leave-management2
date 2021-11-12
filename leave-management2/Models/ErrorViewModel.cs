1a3b1a82d87f1b69995cb3528fff3fc781264aba
lherman <lherman@KBPDEVLHERM1839>
2021-11-12 10:17:32 -0500

    Add project files.

---
+++ leave-management2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace leave_management2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
