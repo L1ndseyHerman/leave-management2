83f62d6ab1b1dda742085d8c32ff767ab290ed16
L1ndseyHerman <lherman21@jcu.edu>
2021-12-11 18:50:45 -0500

    Caught up to vid125: using another design pattern besides the Repository Pattern: the Generic Repository Pattern.

--- leave-management2/Contracts/IRepositoryBase.cs
+++ leave-management2/Contracts/IRepositoryBase.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 
 namespace leave_management2.Contracts
@@ -21,5 +22,27 @@ namespace leave_management2.Contracts
         Task<bool> Update(T entity);
         Task<bool> Delete(T entity);
         Task<bool> Save();
+    }
+
+    public interface IGenericRepository<T> where T : class
+    {
+        //  "Expression" is the data type for a lambda expression.
+        //  Lambda expressions like: q => q.Id == 20
+        //  q is "T", boolean is the bool.
+        //  If the boolean is not present, put default of null.
+
+        //  The second param could be for like: q => q.OrderByDescending(q => q.Id)
+
+        //  3rd param for like including 1 table n not the other or whatevs.
+        Task<IList<T>> FindAll(
+            Expression<Func<T, bool>> expression = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+            List<string> includes = null
+        );
+        Task<T> Find(Expression<Func<T, bool>> expression, List<string> includes = null);
+        Task<bool> isExists(Expression<Func<T, bool>> expression = null);
+        Task Create(T entity);
+        void Update(T entity);
+        void Delete(T entity);
     }
 }
---
+++ leave-management2/Repository/GenericRepository.cs
@@ -0,0 +1,96 @@
+﻿using leave_management2.Contracts;
+using leave_management2.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace leave_management2.Repository
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _context;
+        //  New! w the GenericRepository only.
+        private readonly DbSet<T> _db;
+
+        public GenericRepository(ApplicationDbContext context)
+        {
+            _context = context;
+            //  New too! T = some generic class, aka any class that is passed in is fine.
+            _db = _context.Set<T>();
+        }
+
+        public async Task Create(T entity)
+        {
+            await _db.AddAsync(entity);
+        }
+
+        public void Delete(T entity)
+        {
+             _db.Remove(entity);
+        }
+
+        //  The expression would be like .Where(q => q.Days > 21)
+        //  so FindAll(q => q.Days > 21)
+        public async Task<IList<T>> FindAll(
+            Expression<Func<T, bool>> expression = null, 
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, 
+            List<string> includes = null
+            )
+        {
+            IQueryable<T> query = _db;
+
+            if (expression != null)
+            {
+                query = query.Where(expression);
+            }
+
+            if (includes != null)
+            {
+                foreach (var table in includes)
+                {
+                    query = query.Include(table);
+                }
+            }
+
+            if (orderBy != null)
+            {
+                query = orderBy(query);
+            }
+
+            return await query.ToListAsync();
+
+        }
+
+        public async Task<T> Find(Expression<Func<T, bool>> expression, List<string> includes = null)
+        {
+            IQueryable<T> query = _db;
+
+            //  includes would have a list of strings of the table names.
+            if (includes != null)
+            {
+                foreach (var table in includes)
+                {
+                    query = query.Include(table);
+                }
+            }
+
+            return await query.FirstOrDefaultAsync(expression);
+        }
+
+        public async Task<bool> isExists(Expression<Func<T, bool>> expression = null)
+        {
+            //  Normally, need to parse an IQueryable to an IList or an IEnumerable for other operations.
+            IQueryable<T> query = _db;
+            //  The "expression" will be something like q => q.Id == 10
+            return await query.AnyAsync(expression);
+        }
+
+        public void Update(T entity)
+        {
+            _db.Update(entity);
+        }
+    }
+}
